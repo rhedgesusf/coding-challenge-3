e05c59727fc1b02c08e75a230d9c539b5bb9fcb8
Riley Hedges <rhedges1@usf.edu>
2025-01-28 18:03:33 -0500

    Task 3 - Employee Performance Tracking

--- cc_3.js
+++ cc_3.js
@@ -14,6 +14,42 @@ console.log("Added Price", prices);
 prices.shift();
 console.log("Final Prices", prices);
 
+///////////////////////////////////////
+// Task 2: Modifying Customer Orders //
+///////////////////////////////////////
+
+console.log('--------------------------------------');
+console.log('Task 2: Modifying Customer Orders');
+
+let orders = [51, 84, 11, 95, 107];
+console.log("Initial Orders", orders);
+
+orders[2] += 5;
+console.log("Added 5 to third order", orders);
+
+let ordersTotal = orders.reduce((sum, quantity) => sum + quantity, 0);
+console.log("Total order quantity", ordersTotal);
+
+///////////////////////////////////////////
+// Task 3: Employee Performance Tracking //
+///////////////////////////////////////////
+
+console.log('--------------------------------------');
+console.log('Task 3: Employee Performance Tracking');
+
+let employee = {
+    name: "Riley Hedges",
+    role: "Intern",
+    performanceScore: 81,
+    isActive: true
+};
+console.log("Initial Employee Info", employee);
+
+employee.performanceScore = 85;
+console.log("Updated Employee Performance Score", employee);
+
+employee.promotionEligible = false;
+console.log("Added Promotion Eligibility", employee);
 
 
 
