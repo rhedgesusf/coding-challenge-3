76592e047b63f356811092a8b7bd0e0fd942496f
Riley Hedges <rhedges1@usf.edu>
2025-01-28 17:55:39 -0500

    Task 1 - Product Price Management (I thought I committed it)

--- cc_3.js
+++ cc_3.js
@@ -14,20 +14,6 @@ console.log("Added Price", prices);
 prices.shift();
 console.log("Final Prices", prices);
 
-///////////////////////////////////////
-// Task 2: Modifying Customer Orders //
-///////////////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 2: Modifying Customer Orders');
-
-let orders = [51, 84, 11, 95, 107];
-console.log("Initial Orders", orders);
-
-orders[2] += 5;
-console.log("Added 5 to third order", orders);
-
-let ordersTotal = orders.reduce((sum, quantity) => sum + quantity, 0);
-console.log("Total order quantity", ordersTotal);
 
 
